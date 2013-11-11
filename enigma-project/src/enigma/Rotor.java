847e4cf3c442504f8f8a71c6f1f4a4ed08d7e61d
bmwinstead <bmwinstead@gmail.com>
2013-11-11 18:26:09 -0500

    Changed to Walter's CA_Rotor.

--- enigma-project/src/enigma/Rotor.java
+++ enigma-project/src/enigma/Rotor.java
@@ -1,125 +1,72 @@
 package enigma;
 
+//This implementation uses two substitution databases for ensuring that both forward
+//and reverse encryption is done in O(1) time.
 public class Rotor {
-	/**
-	 * Essentially the wiring of the Rotor.
-	 */
-	private char[] charMap;
-	/**
-	 * The current charMap that takes into account the stepping function. Only
-	 * different from charMap to facilitate encrypting multiple messages without
-	 * having to re-initialize the machine.
-	 */
-	private char[] curCharMap;
-	/**
-	 * The current alphabet.
-	 */
-	private static char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-			'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-			'V', 'W', 'X', 'Y', 'Z' };
-	/**
-	 * The character at which it will cause the next Rotor to step.
-	 */
-	public char stepChar;
-	/**
-	 * Defines the Rotor's position in the Enigma machine.
-	 */
-	public int position;
 
-	public Rotor(char[] cMap, char sChar, int position) {
-		charMap = cMap;
-		stepChar = sChar;
-		curCharMap = charMap;
-	}
-
-	/**
-	 * Applies the current transformation.
-	 * 
-	 * @param c
-	 *            the character to be transformed
-	 * @return the transformed character
-	 */
-	public char applyTransformation(char c) {
-		int pos = mapCharToInt(c);
-		if (pos != -1)
-			return curCharMap[pos];
-		return '-';
-	}
-
-	/**
-	 * Helper function, used to map a character to its array position in the
-	 * alphabet array.
-	 * 
-	 * @param c
-	 *            the character to be found in the alphabet array.
-	 * @return the position of the character in the alphabet array.
-	 */
-	private static int mapCharToInt(char c) {
-		int toReturn = -1;
-
-		for (int i = 0; i < alphabet.length; i++)
-			if (alphabet[i] == Character.toUpperCase(c))
-				toReturn = i;
-		return toReturn;
-	}
-
-	/**
-	 * Helper function, used to increment one individual character, taking into
-	 * account rollover.
-	 * 
-	 * @param c
-	 *            - the character to increment
-	 * @return the incremented character
-	 */
-	private char step(char c) {
-		int a = mapCharToInt(c);
-		if (a == 25)
-			return alphabet[0];
-		return alphabet[a + 1];
-	}
-
-	/**
-	 * Causes the Rotor to "step", by advancing each entry in curCharMap by one,
-	 * with rollover so Z becomes A.
-	 */
-	public void step() {
-		char[] temp = new char[26];
-		for (int i = 0; i < curCharMap.length; i++) {
-			temp[i] = step(curCharMap[i]);
+	private final char[] forwardWiring;
+	private final char[] reverseWiring;
+	
+	private int stepOffset;				// Rotor offset (Grundstellung)
+	private int ringSetting;			// Ring offset (Ringstellung)
+	private final char[] notchPositions;
+	private final int size;             // Substitution alphabet size for easy reading.
+	
+	public Rotor(String code, char[] newNotch) {
+		size = code.length();
+		notchPositions = newNotch;
+		forwardWiring = new char[size];
+		reverseWiring = new char[size];
+		setRingPosition('A');
+		
+		char[] chars = code.toUpperCase().toCharArray();
+		
+		for (int index = 0; index < size; index++) {
+			forwardWiring[index] = chars[index];
+			reverseWiring[chars[index] - 'A'] = (char)('A' + index);
 		}
-		curCharMap = temp;
 	}
 
-	/**
-	 * Causes the Rotor to reset, to facilitate encrypting multiple messages
-	 * with the same key.
-	 */
-	public void reset() {
-		curCharMap = charMap;
-	}
-
-	/**
-	 * Returns a copy of the curCharMap. Currently only used for testing
-	 * purposes.
-	 * 
-	 * @return a copy of the curCharMap
-	 */
-	public char[] getCurCharMap() {
-		char[] temp = new char[curCharMap.length];
-		for (int i = 0; i < temp.length; i++)
-			temp[i] = curCharMap[i];
+	public char[] getNotchPosition() {
+		char[] temp = new char[2];
+			temp[0] = (char)(notchPositions[0] - 1);
+			temp[1] = (char)(notchPositions[1] - 1);
 		return temp;
 	}
-
-	/**
-	 * Returns a copy of the charMap. Currently only used for testing purposes.
-	 * 
-	 * @return a copy of the charMap
-	 */
-	public char[] getCharMap() {
-		char[] temp = new char[charMap.length];
-		for (int i = 0; i < temp.length; i++)
-			temp[i] = charMap[i];
-		return temp;
+	
+	public char getPosition() {
+		return (char)('A' + stepOffset);
+	}
+	
+	public void setRingPosition(char newPosition) {
+		ringSetting = Character.toUpperCase(newPosition) - 'A';
+	}
+	
+	public void setStartPosition(char newPosition) {
+		stepOffset = Character.toUpperCase(newPosition) - 'A';
+	}
+	
+	public boolean cycleRotor() {
+		stepOffset = (stepOffset + 1) % forwardWiring.length;	// Allows wrap-around.
+		
+		if(((notchPositions[0] - 'A') == stepOffset) || (notchPositions[1] - 'A') == stepOffset)
+			return true;
+		return false;	// Returns if the rotor is in the notch position.
+	}
+	
+	public char forwardEncrypt(char letter) {
+		int letterIndex = Character.toUpperCase(letter) - 'A';
+		int offset = stepOffset - ringSetting;
+		int rotorAdjust = (size + letterIndex + offset) % size;
+		int resultOffset = (size + forwardWiring[rotorAdjust] - 'A' - offset) % size;
+		return (char)('A' + resultOffset);
+	}
+	
+	public char reverseEncrypt(char letter) {
+		int letterIndex = Character.toUpperCase(letter) - 'A';
+		int offset = stepOffset - ringSetting;
+		int rotorAdjust = (size + letterIndex + offset) % size;
+		int resultOffset = (size + reverseWiring[rotorAdjust] - 'A' - offset) % size;
+		return (char)('A' + resultOffset);
 	}
 }
