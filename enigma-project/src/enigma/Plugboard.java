07636dd8af15c1bb69b5d41fe59e6d5aa6dab354
ikleyjl <ikleyjl@gmail.com>
2013-11-19 19:54:34 -0500

    Unit test for the Plugboard (only), comments added to Plugboard, small change to prevent Out of Bounds when given an odd-lengthed String.

--- enigma-project/src/enigma/Plugboard.java
+++ enigma-project/src/enigma/Plugboard.java
@@ -5,20 +5,57 @@ public class Plugboard {
 	private char[][] plugboard;
 	private int numPairs;
 	
+	/**
+	 * Constructor. Accepts a string of characters and creates a simple two-way
+	 * replacement cipher. The string should be formatted in pairs with no
+	 * spaces or punctuation. For example, initializing with a string of "ABCD"
+	 * would swap all 'A's with 'B's, all 'B's with 'A's, all 'C's with 'D's,
+	 * and all 'D's with 'C's.
+	 * 
+	 * The constructor does not check the capitalization of the input and does
+	 * not remove punctuation, spaces, or invalid characters. The constructor
+	 * does not prevent double-mapping, although the first match will always be
+	 * the one returned.
+	 * 
+	 * @param mapping
+	 *            String to be used to create the two-way substitution. Strings
+	 *            are not checked for validity. 
+	 * 
+	 */
 	public Plugboard(String mapping){
 		char[] cArr = mapping.toCharArray();
 		numPairs = (int) Math.floor(cArr.length/2);
 		plugboard = new char[numPairs][2];
-		for(int i = 0; i < cArr.length; i++){
+		for(int i = 0; i < numPairs * 2; i++){
 			int j = (int) Math.floor(i/2);
 			plugboard[j][i%2]=cArr[i];
 		}
 	}
 	
+	/**
+	 * Constructor. Builds a two-way substitution cipher out of a
+	 * two-dimensional array. Characters in the first row will be swapped with
+	 * their corresponding characters in the second row, and vice-versa. For
+	 * example, the character in newpb[0][0] will get swapped with newpb[0][1],
+	 * and vice- versa.
+	 * 
+	 * @param newpb
+	 *            Two-dimensional array of characters representing the plugboard
+	 *            configuration. Input is not checked for validity.
+	 */
 	public Plugboard(char[][] newpb){
 		plugboard = newpb;
 	}
 	
+	/**
+	 * Checks a character to see if it's a swap-character. If so, it returns the
+	 * substitution character. If not, it returns the initial character.
+	 * 
+	 * @param c
+	 *            Character to be checked for substitution.
+	 * @return The substitution character if there is a match. If not, return is
+	 *         the original character.
+	 */
 	public char matchChar(char c){
 		for(int i = 0; i < plugboard.length;i++){
 			if(plugboard[i][0] == c)
