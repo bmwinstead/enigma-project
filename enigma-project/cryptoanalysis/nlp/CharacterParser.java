17268e0308ebcba01f2cbc9c6aa5c2d1fd2cf22c
wadolph <wadolph@hotmail.com>
2013-11-21 03:02:52 -0500

    Initial build.

---
+++ enigma-project/cryptoanalysis/nlp/CharacterParser.java
@@ -0,0 +1,80 @@
+/**
+ * CharacterParser.java
+ * @author - walter
+ * @date - Nov 21, 2013
+ * This parser accumulates statistical data from a corpus on indiviudal characters.
+ * 1,2,3, and 4 character grams are analyzed.
+ * Punctuation is left in place.
+ */
+package nlp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author walter
+ *
+ */
+public class CharacterParser {
+	private Corpus database;
+	
+	public CharacterParser(Corpus newCorpus) {
+		database = newCorpus;
+	}
+	
+	public void parseFile(File file) {
+		try {
+			Scanner scanner = new Scanner(file);
+
+			char firstgram = '\0';
+			char secondgram = '\0';
+			char thirdgram = '\0';
+			char fourthgram = '\0';
+			
+			// Parse words.
+			while (scanner.hasNext()) {
+				char[] characters = scanner.next().toLowerCase().toCharArray();
+				
+				for (char character: characters) {
+					fourthgram = thirdgram;
+					thirdgram = secondgram;
+					secondgram = firstgram;
+					firstgram = character;
+					
+					database.addUnigram(String.valueOf(firstgram));
+					
+					if (secondgram != '\0') {
+						String phrase = "" + secondgram + firstgram;
+						
+						database.addBigram(phrase);
+						
+						if (thirdgram != '\0') {
+							phrase = "" + thirdgram + secondgram + firstgram;
+							
+							database.addTrigram(phrase);
+							
+							if (fourthgram != '\0') {
+								phrase = "" + fourthgram + thirdgram + secondgram + firstgram;
+								
+								database.addQuadgram(phrase);
+							}
+						}
+					}
+				}
+				
+				
+				
+			} // End while
+			
+			scanner.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Parsing complete; sort for ready use.
+		database.sortDatabase();
+	}
+}
