5a610b1228447bd4e77c37febd08a5b577482084
EOhlmacher <eohlmacher@gmail.com>
2013-11-27 13:48:08 -0500

    Rosa created this awesome plugboard design. Submitting as a separate window for group evaluation.

--- enigma-project/src/main/java/GUI/Enigma.java
+++ enigma-project/src/main/java/GUI/Enigma.java
@@ -10,24 +10,40 @@ import javax.swing.JFrame;
  * 
  * A driver to create the Enigma GUI panel. Currently this works as an app,
  * but the team intends to have both app and applet options in the future. 
- *
+ * 
+ * Current implementation includes new mock ups for rotor panels and
+ * plug board.
  */
 public class Enigma {
 
     public static void main(String[] args) {
-        new Enigma();
+        new Enigma(); //Original GUI
+        //New Rotor Panel GUI
 		RotorPanel r = new RotorPanel();
 		JFrame frame = new JFrame();
 		frame.add(r);
 		frame.pack();
 		frame.setVisible(true);
+		new PlugBoardGUI(); //New PlugBoard GUI
+
     }
     
     public Enigma(){
         
-        //Create the GUI Panel      
+        //Create the original GUI Panel      
         EnigmaGUI frame = new EnigmaGUI();
         frame.setVisible(true);
+
+    }
+    
+    public void PlugBoardGUI(){
+        
+		//New PlugBoard GUI
+		PlugBoardGUI p = new PlugBoardGUI();
+		JFrame plugFrame = new JFrame();
+		plugFrame.add(p);
+		plugFrame.pack();
+		plugFrame.setVisible(true);
 
     }
 }
