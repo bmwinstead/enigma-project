4f267d6b7180226912e7a3eb41f6515200022d23
bmwinstead <bmwinstead@gmail.com>
2013-11-28 04:05:07 -0500

    Added...stuff

--- enigma-project/src/main/java/GUINew/EnigmaGUI.java
+++ enigma-project/src/main/java/GUINew/EnigmaGUI.java
@@ -1,24 +1,34 @@
 package main.java.GUINew;
 import java.awt.GridLayout;
 
+import javax.swing.GroupLayout;
 import javax.swing.JFrame;
 public class EnigmaGUI {
 	private static RotorPanel rotorPanel;
 	private static PlugboardDialog plugboard;
 	private static JFrame mainFrame;
+	private static Lightboard lightboard;
 	private static IOPanel ioPanel;
 	public static void main(String[] args){
 		mainFrame = new JFrame();
-		mainFrame.setLayout(new GridLayout(0,1));
+		GroupLayout frameLayout = new GroupLayout(mainFrame.getContentPane());
+		mainFrame.getContentPane().setLayout(frameLayout);
+		
 		rotorPanel = new RotorPanel();
-		plugboard = new PlugboardDialog();
 		ioPanel = new IOPanel();
-		mainFrame.add(rotorPanel);
-		mainFrame.add(ioPanel);
+		frameLayout.setHorizontalGroup(frameLayout.createParallelGroup()
+				.addComponent(rotorPanel)
+				.addComponent(ioPanel));
+		frameLayout.setVerticalGroup(frameLayout.createSequentialGroup()
+				.addComponent(rotorPanel)
+				.addComponent(ioPanel));
+//		plugboard = new PlugboardDialog();
+//		lightboard = new Lightboard();
+
 		mainFrame.setTitle("CMSC495 Enigma Machine");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.pack();
 		mainFrame.setVisible(true);
-		ioPanel.turnOnLight("testing");
 //		String s = plugboard.displayDialog();
 //		System.out.println(s);
 	}
